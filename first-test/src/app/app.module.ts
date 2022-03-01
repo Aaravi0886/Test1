bf7d7a5c13355d3cfeaa1e8326321c5b9299a274
Aaravi0886 <gaurav.vit08@gmail.com>
2022-03-01 10:07:11 +0530

    Submitting Project in Library

--- first-test/src/app/app.module.ts
+++ first-test/src/app/app.module.ts
@@ -1,16 +1,36 @@
+;
+import { UsersComponent } from './users/users.component';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MessageComponent } from './message/message.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+const appRoutes: Routes=[
+  {path: 'users',component: UsersComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' },
+  {path: 'profile',component: ProfileComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '/users', pathMatch: 'full' }
+  
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponent,
+    UsersComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
